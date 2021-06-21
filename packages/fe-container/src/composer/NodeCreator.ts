7b870af01bd975bc3e86f35dac4101d51b3ae861
iLTeoooD <matteo.dazzi@mia-platform.eu>
2021-06-21 09:22:28 +0200

    View engine with tests

--- packages/fe-container/src/composer/NodeCreator.ts
+++ packages/fe-container/src/composer/NodeCreator.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import {Configuration} from '@mia-platform/core'
 
 const rowStyle = 'display: "flex"; flex-direction: "column"'
@@ -7,14 +22,12 @@ type CreateFunction = (configuration: Configuration) => HTMLDivElement
 
 const createRow: CreateFunction = (configuration: Configuration) => {
   const row = document.createElement('div')
-  // @ts-ignore
   row.setAttribute('style', rowStyle)
   return row
 }
 
 const createColumn: CreateFunction = (configuration: Configuration) => {
   const column = document.createElement('div')
-  // @ts-ignore
   column.setAttribute('style', columnStyle)
   return column
 }
--- packages/fe-container/src/composer/__tests__/ViewEngine.test.ts
+++ packages/fe-container/src/composer/__tests__/ViewEngine.test.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import viewEngine from '../ViewEngine'
 
 describe('ViewEngine tests', () => {
@@ -5,14 +20,20 @@ describe('ViewEngine tests', () => {
     const type: 'row' = 'row'
     const rowConfig = {type}
     const viewBuilt = viewEngine([rowConfig])
-    expect(viewBuilt.outerHTML).toBe('<div><div style="display: &quot;flex&quot;; flex-direction: &quot;column&quot;"></div></div>')
+    expect(viewBuilt.outerHTML).toBe('<div>' +
+      '<div style="display: &quot;flex&quot;; flex-direction: &quot;column&quot;">' +
+      '</div>' +
+      '</div>')
   })
 
   it('create correctly a column', () => {
     const type: 'column' = 'column'
     const rowConfig = {type}
     const viewBuilt = viewEngine([rowConfig])
-    expect(viewBuilt.outerHTML).toBe('<div><div style="display: &quot;flex&quot;; flex-direction: &quot;row&quot;"></div></div>')
+    expect(viewBuilt.outerHTML).toBe('<div>' +
+      '<div style="display: &quot;flex&quot;; flex-direction: &quot;row&quot;">' +
+      '</div>' +
+      '</div>')
   })
 
   it('create correctly a column inside a row', () => {
@@ -20,6 +41,39 @@ describe('ViewEngine tests', () => {
     const row: 'row' = 'row'
     const rowConfig = {type: row, content: [{type: column}]}
     const viewBuilt = viewEngine([rowConfig])
-    expect(viewBuilt.outerHTML).toBe('<div><div style="display: &quot;flex&quot;; flex-direction: &quot;column&quot;"><div style="display: &quot;flex&quot;; flex-direction: &quot;row&quot;"></div></div></div>')
+    expect(viewBuilt.outerHTML).toBe('<div>' +
+      '<div style="display: &quot;flex&quot;; flex-direction: &quot;column&quot;">' +
+      '<div style="display: &quot;flex&quot;; flex-direction: &quot;row&quot;">' +
+      '</div>' +
+      '</div>' +
+      '</div>')
+  })
+
+  it('create correctly an element inside a column inside a row', () => {
+    const column: 'column' = 'column'
+    const row: 'row' = 'row'
+    const element: 'element' = 'element'
+    const rowConfig = {
+      type: row,
+      content: [{
+        type: column,
+        content: [{
+          type: element,
+          tag: 'button',
+          url: 'https://google.it',
+          config: {'attribute-a': 'value-a'}
+        }]
+      }]
+    }
+    document.appendChild = jest.fn()
+    const viewBuilt = viewEngine([rowConfig])
+    expect(viewBuilt.outerHTML).toBe('<div>' +
+      '<div style="display: &quot;flex&quot;; flex-direction: &quot;column&quot;">' +
+      '<div style="display: &quot;flex&quot;; flex-direction: &quot;row&quot;">' +
+      '<button attribute-a="value-a"></button>' +
+      '</div>' +
+      '</div>' +
+      '</div>')
+    expect(document.appendChild).toHaveBeenCalledTimes(1)
   })
 })
