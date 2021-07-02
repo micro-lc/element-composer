0552087a5ca4d653d27e6a29de298344aeed5cb8
iLTeoooD <matteo.dazzi@mia-platform.eu>
2021-07-02 16:21:53 +0200

    Cleaned

--- mock-server/api/v1/microlc/authentication/get.js
+++
@@ -1,9 +0,0 @@
-module.exports = (request, response) => {
-  response
-    .delay(1000)
-    .send({
-      isAuthNecessary: true,
-      userInfoUrl: '/api/v1/microlc/user',
-      userLogoutUrl: '/api/v1/microlc/user/logout'
-    })
-}
