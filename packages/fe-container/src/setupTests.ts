22b27a7f8863de6907744c789864953109394579
iLTeoooD <matteo.dazzi@mia-platform.eu>
2021-07-23 15:33:07 +0200

    Removed unused dependencies

--- packages/fe-container/src/setupTests.ts
+++ packages/fe-container/src/setupTests.ts
@@ -14,14 +14,3 @@
  * limitations under the License.
  */
 import '@testing-library/jest-dom'
-
-jest.mock('react-intl', () => {
-  const strings = require('./strings/locales/en.json')
-  const reactIntl = jest.requireActual('react-intl')
-  const intl = reactIntl.createIntl({locale: 'en', messages: strings})
-
-  return {
-    ...reactIntl,
-    useIntl: () => intl
-  }
-})
